f79cb4c52462bb41f9cca35259ce63db0a6e8fa5
Mikkel Bentzrud Rasch <mikkel@bentzrud.no>
2024-02-07 11:21:39 +0100

    init commit

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,79 +1,57 @@
-<script setup>
+<template>
+  <h3>Contact:</h3>
+  <form @submit.prevent="sendForm">
+    <BasicInput v-model="event.name" label="Name" type="text" />
+    <BasicInput v-model="event.email" label="E-Mail" type="email" />
+    <span v-if="emailError" style="color: red;">{{ emailError }}</span>
+    <BasicInput v-model="event.message" label="Message" type="text" />
+    <span v-if="messageError" style="color: red;">{{ messageError }}</span>
+    <button type="submit">SUBMIT</button>
+  </form>
+</template>
 
+<script setup>
 import BasicInput from "@/components/BasicInput.vue";
 import axios from "axios";
+
+const validateEmail = (email) => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+};
 </script>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       event: {
-        name: '',
-        email: '',
-        message: ''
-      }
-    }
+        name: "",
+        email: "",
+        message: ""
+      },
+      emailError: "",
+      messageError: ""
+    };
   },
   methods: {
-    sendForm () {
+    sendForm() {
+      this.emailError = "";
+      this.messageError = "";
+
+      if (!validateEmail(this.event.email)) {
+        this.emailError = "Please enter a valid email address.";
+        return;
+      }
+
+      if (this.event.message.trim() === "") {
+        this.messageError = "Please enter a message.";
+        return;
+      }
+
+      axios.po
+
 
     }
   }
-}
+};
 </script>
-
-<template>
-
-  <h3>Contact: </h3>
- <form @submit.prevent="sendForm">
-   <BasicInput
-       v-model="event.name"
-       label="Title"
-       type="text"/>
-
-   <BasicInput
-       v-model="event.email"
-       label="E-Mail "
-       type="text"/>
-   <BasicInput
-       v-model="event.message"
-       label="Message"
-       type="text"/>
-
-   <button type="submit">SUBMIT</button>
-
- </form>
-</template>
-
-<style scoped>
-
-.container {
-  overflow-y: auto;
-  padding: 10px;
-  display: flex;
-  flex-grow: 1;
-  flex-direction: column;
-  margin: 5px;
-}
-
-.label {
-  font-size: 16px;
-  font-family: "Arial";
-  margin-bottom: 0;
-}
-
-.field {
-  font-size: 20px;
-  font-family: "Arial";
-  color: black;
-  width: 200px;
-  margin-top: 2px;
-  margin-bottom: 5px;
-}
-
-
-
-
-
-</style>
